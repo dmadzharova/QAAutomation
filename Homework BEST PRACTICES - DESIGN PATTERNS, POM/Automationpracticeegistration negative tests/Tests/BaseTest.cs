d4a071e42ee464d5d4721abe0084cf38ee4ea0b7
dmadzharova <daniella.madzharova@gmail.com>
2019-10-19 23:15:11 +0300

    Adding support of SeleniumGrid

--- Homework BEST PRACTICES - DESIGN PATTERNS, POM/Automationpracticeegistration negative tests/Tests/BaseTest.cs
+++ Homework BEST PRACTICES - DESIGN PATTERNS, POM/Automationpracticeegistration negative tests/Tests/BaseTest.cs
@@ -1,11 +1,10 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Text;
 
 namespace Homework.Pages
 {
@@ -17,7 +16,16 @@ namespace Homework.Pages
         [OneTimeSetUp]
         public void InitializeTests()
         {
-            Driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            //Config for SeleniumGrid
+            ChromeOptions options = new ChromeOptions();
+            options.PlatformName = "windows";
+            options.BrowserVersion = "77.0";
+
+            Driver = new RemoteWebDriver(new Uri("http://192.168.1.103:4444/wd/hub"), options.ToCapabilities(), TimeSpan.FromSeconds(10));
+            Driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+
+            //Config for local run on Chrome
+            //Driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
             Driver.Manage().Window.Maximize();
             Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
